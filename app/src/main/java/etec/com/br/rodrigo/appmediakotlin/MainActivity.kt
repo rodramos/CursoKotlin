0d655cdbfeb522579cbf8ba3ddf8b93d47364e41
Rodrigo Ramos <gordramos@yahoo.com.br>
2021-10-26 22:19:17 -0300

    Projeto curso koltin

--- app/src/main/java/etec/com/br/rodrigo/appmediakotlin/MainActivity.kt
+++ app/src/main/java/etec/com/br/rodrigo/appmediakotlin/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var rdbEtim:RadioButton
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
